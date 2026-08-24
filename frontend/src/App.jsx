d6edce5482c39582f65dfef8ad611cf1c2a9b4c6
Malshi Herath <herathmalshi@gmail.com>
2026-08-24 12:47:34 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import About from './pages/About'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Appointments from './pages/Appointments'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/docters' element={<Doctors />} />
+        <Route path='/docters/:speciality' element={<Doctors />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/appointments/:docId' element={<Appointments />} />
+        <Route path='/appointment/:docId' element={<Appointments />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
